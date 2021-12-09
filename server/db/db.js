63823dbfbc6bcefc691a8a4b608120ffa69ffc4a
ohzecodes <49818965+ohzecodes@users.noreply.github.com>
2021-12-09 10:15:33 -0800

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,20 @@
 require("dotenv");
+// const Sequelize = require("sequelize");
+// const { dbname, dbuser, dbpass, dbhost, dbport } = process.env;
+// const db = new Sequelize({
+//   database: dbname,
+//   username: dbuser,
+//   password: dbpass,
+//   host: dbhost,
+//   port: dbport,
+//   dialect: "postgres",
+//   logging: false,
+// });
+const { Database_URL } = process.env;
+
 const Sequelize = require("sequelize");
-const { dbname, dbuser, dbpass, dbhost, dbport } = process.env;
-const db = new Sequelize({
-  database: dbname,
-  username: dbuser,
-  password: dbpass,
-  host: dbhost,
-  port: dbport,
-  dialect: "postgres",
+
+const db = new Sequelize(Database_URL, {
   logging: false,
 });
 
@@ -17,5 +24,6 @@ try {
 } catch (error) {
   console.error("Unable to connect to the database:", error);
 }
-
 module.exports = db;
+
+// DELETE FROM messages WHERE "createdAt" > '2021-12-02 07:24:56';
